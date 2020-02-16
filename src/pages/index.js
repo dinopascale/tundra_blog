4ad22905ce33aed3926f78b7c6cf915c12b40df0
Dino Pascale <dino.pascale@gmail.com>
2020-02-16 15:45:11 +0100

    added list of post in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,34 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default ({data}) => {
+    const {allMarkdownRemark: {edges} } = data;
+    return (
+        <ul>
+            {
+                edges.map(({node}) => 
+                <li key={node.id}>
+                    <Link to={node.frontmatter.path}>{node.frontmatter.title}</Link>
+                </li>)
+            }
+        </ul>
+    )
+}
+
+export const homePageQuery = graphql`
+    {
+        allMarkdownRemark {
+            edges {
+                node {
+                    id
+                    excerpt
+                    frontmatter {
+                        date
+                        path
+                        title
+                    }
+                }
+            }
+        }
+    }
+`
